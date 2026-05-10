179afc4168996d8daecd3b260c3776c467f07015
Rungrawee <jelly_pop3.2@hotmail.co.th>
2026-05-10 16:34:44 +0700

    add simulator service

---
+++ simulator/index.js
@@ -0,0 +1,28 @@
+const axios = require("axios");
+
+function generateSensorData() {
+  return {
+    deviceId: 1,
+    temperature: Math.floor(Math.random() * 100),
+    vibration: Math.floor(Math.random() * 10),
+    timestamp: new Date().toISOString(),
+  };
+}
+
+async function sendSensorData() {
+  const data = generateSensorData();
+
+  try {
+    const response = await axios.post(
+      "http://localhost:5153/api/sensor-data",
+      data
+    );
+
+    console.log("Sent:", data);
+    console.log("Response:", response.data);
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+}
+
+setInterval(sendSensorData, 5000);
